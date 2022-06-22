bb300ab522ae6f374cfb943937c6a30118660032
Tomáš Kafka <tk@tomaskafka.com>
2022-06-22 14:31:49 +0200

    Try to add macOS 11 support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ let package = Package(
     name: "SwiftUIPageView",
     platforms: [
         .iOS(.v14),
-        .watchOS(.v7)
+        .watchOS(.v7),
+        .macOS(.v11),
     ],
     products: [
         .library(
